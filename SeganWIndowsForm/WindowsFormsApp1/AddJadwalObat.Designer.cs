2c0ff81ec0f9cba467958cf6e1af7a6c46d66cdc
akbarsigit <akbarsigit3@gmail.com>
2022-11-19 20:01:35 +0700

    feat: add active obat

--- SeganWIndowsForm/WindowsFormsApp1/AddJadwalObat.Designer.cs
+++ SeganWIndowsForm/WindowsFormsApp1/AddJadwalObat.Designer.cs
@@ -31,17 +31,19 @@ namespace WindowsFormsApp1
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddJadwalObat));
             this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.submitDrugAct = new System.Windows.Forms.Button();
+            this.deleteDrugAct = new System.Windows.Forms.Button();
             this.updateDrugAct = new System.Windows.Forms.Button();
             this.ObatDropdown = new Bunifu.Framework.UI.BunifuDropdown();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tbTanggal = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.deleteDrugAct = new System.Windows.Forms.Button();
-            this.submitDrugAct = new System.Windows.Forms.Button();
+            this.tbDosis = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbInterval = new System.Windows.Forms.TextBox();
             this.bunifuGradientPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,17 +51,19 @@ namespace WindowsFormsApp1
             // 
             this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
             this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.label5);
+            this.bunifuGradientPanel1.Controls.Add(this.tbInterval);
             this.bunifuGradientPanel1.Controls.Add(this.submitDrugAct);
             this.bunifuGradientPanel1.Controls.Add(this.deleteDrugAct);
             this.bunifuGradientPanel1.Controls.Add(this.updateDrugAct);
             this.bunifuGradientPanel1.Controls.Add(this.ObatDropdown);
             this.bunifuGradientPanel1.Controls.Add(this.label4);
-            this.bunifuGradientPanel1.Controls.Add(this.textBox4);
+            this.bunifuGradientPanel1.Controls.Add(this.tbTanggal);
             this.bunifuGradientPanel1.Controls.Add(this.label3);
             this.bunifuGradientPanel1.Controls.Add(this.label2);
             this.bunifuGradientPanel1.Controls.Add(this.label1);
-            this.bunifuGradientPanel1.Controls.Add(this.textBox2);
-            this.bunifuGradientPanel1.Controls.Add(this.textBox1);
+            this.bunifuGradientPanel1.Controls.Add(this.tbDosis);
+            this.bunifuGradientPanel1.Controls.Add(this.tbName);
             this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(104)))), ((int)(((byte)(159)))));
             this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(235)))), ((int)(((byte)(212)))));
             this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(235)))), ((int)(((byte)(212)))));
@@ -67,15 +71,40 @@ namespace WindowsFormsApp1
             this.bunifuGradientPanel1.Location = new System.Drawing.Point(1, 1);
             this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
             this.bunifuGradientPanel1.Quality = 10;
-            this.bunifuGradientPanel1.Size = new System.Drawing.Size(506, 479);
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(506, 616);
             this.bunifuGradientPanel1.TabIndex = 99999;
+            // 
+            // submitDrugAct
+            // 
+            this.submitDrugAct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(104)))), ((int)(((byte)(159)))));
+            this.submitDrugAct.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitDrugAct.ForeColor = System.Drawing.Color.White;
+            this.submitDrugAct.Location = new System.Drawing.Point(122, 537);
+            this.submitDrugAct.Name = "submitDrugAct";
+            this.submitDrugAct.Size = new System.Drawing.Size(237, 36);
+            this.submitDrugAct.TabIndex = 19;
+            this.submitDrugAct.Text = "Submit";
+            this.submitDrugAct.UseVisualStyleBackColor = false;
+            this.submitDrugAct.Click += new System.EventHandler(this.submitDrugAct_Click);
+            // 
+            // deleteDrugAct
+            // 
+            this.deleteDrugAct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(78)))), ((int)(((byte)(96)))));
+            this.deleteDrugAct.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteDrugAct.ForeColor = System.Drawing.Color.White;
+            this.deleteDrugAct.Location = new System.Drawing.Point(252, 482);
+            this.deleteDrugAct.Name = "deleteDrugAct";
+            this.deleteDrugAct.Size = new System.Drawing.Size(166, 36);
+            this.deleteDrugAct.TabIndex = 18;
+            this.deleteDrugAct.Text = "Delete";
+            this.deleteDrugAct.UseVisualStyleBackColor = false;
             // 
             // updateDrugAct
             // 
             this.updateDrugAct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(99)))), ((int)(((byte)(255)))));
             this.updateDrugAct.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.updateDrugAct.ForeColor = System.Drawing.Color.White;
-            this.updateDrugAct.Location = new System.Drawing.Point(76, 357);
+            this.updateDrugAct.Location = new System.Drawing.Point(76, 482);
             this.updateDrugAct.Name = "updateDrugAct";
             this.updateDrugAct.Size = new System.Drawing.Size(161, 36);
             this.updateDrugAct.TabIndex = 17;
@@ -106,17 +135,17 @@ namespace WindowsFormsApp1
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Location = new System.Drawing.Point(73, 269);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.Size = new System.Drawing.Size(132, 17);
             this.label4.TabIndex = 15;
-            this.label4.Text = "Dosis";
+            this.label4.Text = "Dikonsumsi tanggal";
             // 
-            // textBox4
+            // tbTanggal
             // 
-            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(76, 289);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(342, 29);
-            this.textBox4.TabIndex = 14;
+            this.tbTanggal.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTanggal.Location = new System.Drawing.Point(76, 289);
+            this.tbTanggal.Name = "tbTanggal";
+            this.tbTanggal.Size = new System.Drawing.Size(342, 29);
+            this.tbTanggal.TabIndex = 14;
             // 
             // label3
             // 
@@ -154,51 +183,47 @@ namespace WindowsFormsApp1
             this.label1.TabIndex = 11;
             this.label1.Text = "Schedule Name";
             // 
-            // textBox2
+            // tbDosis
             // 
-            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(76, 216);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(342, 29);
-            this.textBox2.TabIndex = 10;
+            this.tbDosis.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDosis.Location = new System.Drawing.Point(76, 216);
+            this.tbDosis.Name = "tbDosis";
+            this.tbDosis.Size = new System.Drawing.Size(342, 29);
+            this.tbDosis.TabIndex = 10;
             // 
-            // textBox1
+            // tbName
             // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(76, 82);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(342, 29);
-            this.textBox1.TabIndex = 9;
+            this.tbName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbName.Location = new System.Drawing.Point(76, 82);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(342, 29);
+            this.tbName.TabIndex = 9;
             // 
-            // deleteDrugAct
+            // label5
             // 
-            this.deleteDrugAct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(78)))), ((int)(((byte)(96)))));
-            this.deleteDrugAct.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.deleteDrugAct.ForeColor = System.Drawing.Color.White;
-            this.deleteDrugAct.Location = new System.Drawing.Point(252, 357);
-            this.deleteDrugAct.Name = "deleteDrugAct";
-            this.deleteDrugAct.Size = new System.Drawing.Size(166, 36);
-            this.deleteDrugAct.TabIndex = 18;
-            this.deleteDrugAct.Text = "Delete";
-            this.deleteDrugAct.UseVisualStyleBackColor = false;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(73, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Interval";
             // 
-            // submitDrugAct
+            // tbInterval
             // 
-            this.submitDrugAct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(104)))), ((int)(((byte)(159)))));
-            this.submitDrugAct.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.submitDrugAct.ForeColor = System.Drawing.Color.White;
-            this.submitDrugAct.Location = new System.Drawing.Point(122, 412);
-            this.submitDrugAct.Name = "submitDrugAct";
-            this.submitDrugAct.Size = new System.Drawing.Size(237, 36);
-            this.submitDrugAct.TabIndex = 19;
-            this.submitDrugAct.Text = "Submit";
-            this.submitDrugAct.UseVisualStyleBackColor = false;
+            this.tbInterval.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbInterval.Location = new System.Drawing.Point(76, 359);
+            this.tbInterval.Name = "tbInterval";
+            this.tbInterval.Size = new System.Drawing.Size(342, 29);
+            this.tbInterval.TabIndex = 20;
             // 
             // AddJadwalObat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 481);
+            this.ClientSize = new System.Drawing.Size(506, 615);
             this.Controls.Add(this.bunifuGradientPanel1);
             this.Name = "AddJadwalObat";
             this.Text = "AddJadwalObat";
@@ -214,13 +239,15 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button updateDrugAct;
         private Bunifu.Framework.UI.BunifuDropdown ObatDropdown;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox tbTanggal;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbDosis;
+        private System.Windows.Forms.TextBox tbName;
         private System.Windows.Forms.Button submitDrugAct;
         private System.Windows.Forms.Button deleteDrugAct;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbInterval;
     }
 }
--- SeganWIndowsForm/WindowsFormsApp1/AddJadwalObat.cs
+++ SeganWIndowsForm/WindowsFormsApp1/AddJadwalObat.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace WindowsFormsApp1
 {
     public partial class AddJadwalObat : Form
     {
+        NpgsqlConnection con;
+        NpgsqlCommand cmd;
+        private NpgsqlDataReader dr;
+        private DataTable obatList;
         public AddJadwalObat()
         {
             InitializeComponent();
+            dbConnection dbConnect = new dbConnection();
+            (con, cmd) = dbConnect.InitializeConnection();
+        }
+
+        private void obatSeeding()
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = string.Format(
+                "SELECT obat_nama FROM obat;");
+
+
+            dr = cmd.ExecuteReader();
+            obatList = new DataTable();
+            obatList.Load(dr);
+            //DataRow[] dataRows = obatList;
+            //foreach(string obat in dr)
+            //{
+            //    ObatDropdown.Items.
+            //}
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -25,6 +50,37 @@ namespace WindowsFormsApp1
         private void bunifuThinButton21_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void submitDrugAct_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nama = tbName.Text;
+                string obat = ObatDropdown.Text;
+                string dosis = tbDosis.Text;
+                string interval = tbInterval.Text;
+                string tanggal = tbTanggal.Text;
+
+                if (nama == "" || obat == "" || dosis == "" || interval == "" || tanggal == "")
+                {
+                    MessageBox.Show("Silahkan isi semua data");
+                    return;
+                }
+
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = string.Format(
+                    "INSERT INTO obat (obat_nama, obat_type, obat_desc, obat_image) VALUES ('{0}','{1}','{2}','{3}');", nama, type, desc, gambar);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Berhasil Input Obat");
+                con.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message, "FAIL Insert!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
